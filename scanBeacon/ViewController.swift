d240cf0041ef002b3abcd62aed9477d526194c70
Arlo Erdaka <arlo.erdaka@gmail.com>
2019-07-26 10:09:46 +0700

    Initial Commit

---
+++ scanBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scanBeacon
+//
+//  Created by Arlo Erdaka on 26/07/19.
+//  Copyright © 2019 Arlo Erdaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
